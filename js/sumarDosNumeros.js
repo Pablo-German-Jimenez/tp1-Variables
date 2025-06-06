17edf9d9e15d974db56c8f52a3cccca5de94a0b9
linkin <pablogermanjimenez7@gmail.com>
2025-06-06 16:00:37 -0300

    ejercicio pedir y sumar dos numeritos

---
+++ js/sumarDosNumeros.js
@@ -0,0 +1,9 @@
+/*Realizar una suma de dos números:  Crear un algoritmo que permita al usuario ingresar dos números, realizar la suma de ambos y mostrar el resultado.*/
+let num1 = parseInt(prompt('Vamos a sumar dos numeritos! dale?! Dime el primer número a sumar'));
+let num2 = parseInt(prompt('Ahora dime el segundo numerito a sumar'));
+
+suma= num1+num2;
+if(Number(suma)){
+alert('La suma de ambos es:'+suma)}
+else(alert('Ingresá de nuevo pero numerito rey'));
+
