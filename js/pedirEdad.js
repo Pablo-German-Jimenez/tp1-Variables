a50e47790716b3ddccbe250a1986c9d2dc10e9e8
linkin <pablogermanjimenez7@gmail.com>
2025-06-06 11:02:31 -0300

    pedir edad

---
+++ js/pedirEdad.js
@@ -0,0 +1,8 @@
+//Pedir la edad y decir si es mayor de edad: Crear un algoritmo que pida la edad del usuario y determine
+//si es mayor de edad(18 años o mas).
+const edad = parseInt(prompt('Ingresa tu edad'));
+if(edad>=18){
+    alert('sos mayor de edad')
+}else{
+    alert('sos menor de edad rey')
+}
