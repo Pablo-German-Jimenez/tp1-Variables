b41bc55ad22419774be94f969f1996d615640f0f
linkin <pablogermanjimenez7@gmail.com>
2025-06-09 11:28:16 -0300

    ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,6 @@
+/*9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
+
+let frase = prompt('Dame una frase para buscar las vocales');
+(frase.charAt(2));
+console.log(frase)
