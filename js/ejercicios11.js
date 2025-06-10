50e660fcb5a14ed3a18080241673d1e4f1d6b75d
linkin <pablogermanjimenez7@gmail.com>
2025-06-09 23:30:59 -0300

    comit once

---
+++ js/ejercicios11.js
@@ -0,0 +1,6 @@
+/*11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)*/
+let num= parseInt( prompt('Dame un numerito, veamos si es divisible por 2,3,5 o 7'));
+
+if(num%2===0%3===0%5===0%7===0){
+    alert('El numerito'+ num+'que ingresaste es divisible por 2,3,5 y 7');
+}else{alert('Siga participando:B')}
