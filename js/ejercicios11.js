6c0b73712a2b1f02c3abeed51cbaf73b47452314
linkin <pablogermanjimenez7@gmail.com>
2025-06-12 16:24:48 -0300

    videito tirando un par de pasos

--- js/ejercicios11.js
+++ js/ejercicios11.js
@@ -1,13 +1,65 @@
 /*11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)*/
-let num= parseInt( prompt('Dame un numerito, veamos si es divisible por 2,3,5 o 7'));
+/*let num= parseInt( prompt('Dame un numerito, veamos si es divisible por 2,3,5 o 7'));*/
 
-if(num%2===0){
-    alert('El numero '+num+' es divisible por 2')
+
+const num = parseInt(prompt('Ingrese un numero: '))
+
+if( num % 2 == 0 && num % 3 == 0 && num % 5 == 0 && num % 7 == 0 ){
+    document.writeln('El numero '+num+' es divisible por 2, 3, 5 y 7')
+}else{
+    if( num % 2 == 0 && num % 3 == 0 && num % 5 == 0 ){
+        document.writeln('El numero '+num+' es divisible por 2, 3 y 5')
+    }else{
+        if( num % 2 == 0 && num % 3 == 0 && num % 7 == 0 ){
+        document.writeln('El numero '+num+' es divisible por 2, 3 y 7')
+        }else{
+            if( num % 2 == 0 && num % 5 == 0 && num % 7 == 0 ){
+            document.writeln('El numero '+num+' es divisible por 2, 5 y 7')
+            }else{
+                if( num % 3 == 0 && num % 5 == 0 && num % 7 == 0 ){
+                document.writeln('El numero '+num+' es divisible por 3, 5 y 7')
+                }else{
+                    if( num % 2 == 0 && num % 3 == 0 ){
+                        document.writeln('El numero '+num+' es divisible por 2 y 3')
+                    }else{
+                        if( num % 2 == 0 && num % 5 == 0 ){
+                            document.writeln('El numero '+num+' es divisible por 2 y 5')
+                        }else{
+                            if ( num % 2 == 0 && num % 7 == 0 ){
+                                document.writeln('El numero '+num+' es divisible por 2 y 7')
+                            }else{
+                                if( num % 3 == 0 && num % 5 == 0 ){
+                                    document.writeln('El numero '+num+' es divisible por 3 y 5')
+                                }else{
+                                    if( num % 3 == 0 && num % 7 == 0 ){
+                                        document.writeln('El numero '+num+' es divisible por 3 y 7')
+                                    }else{
+                                        if( num % 5 == 0 && num % 7 == 0 ){
+                                            document.writeln('El numero '+num+' es divisible por 5 y 7')
+                                        }else{
+                                            if( num % 2 == 0 ){
+                                                document.writeln('El numero '+num+' es divisible por 2')
+                                            }else{
+                                                if( num % 3 == 0 ){
+                                                    document.writeln('El numero '+num+' es divisible por 3')
+                                                }else{
+                                                    if( num % 5 == 0 ){
+                                                        document.writeln('El numero '+num+' es divisible por 5')
+                                                    }else{
+                                                        if( num % 7 == 0 ){
+                                                            document.writeln('El numero '+num+' es divisible por 7')
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-else if(num%3===0){
-    alert('El numero '+num+' es divisible por 3')
-}else if(num%5===0){
-    alert('El numero '+num+' es divisible por 5')
-}else if(num%7===0){
-    alert('El numero '+num+' es divisible por 7')
-}else{alert('No un número divisible por 2,3,5 o 7')}
